00d446fab61f5189c2afe1b35b5a5ee95a561827
AJ-505 <abasiono.mbat@gmail.com>
2026-04-29 16:06:46 +0100

    feat(wip): password hashing; fix(user): update id to use a string(uuid)

--- internals/domain/models/user.go
+++ internals/domain/models/user.go
@@ -1,11 +1,15 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
 type User struct {
 	Id        string
 	Name      string
 	Email     string
+	Password  string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
+
--- internals/infrastructure/persistence/gorm/repositories/user_repository.go
+++ internals/infrastructure/persistence/gorm/repositories/user_repository.go
@@ -5,6 +5,7 @@ import (
 	"auptex.com/botnova/internals/infrastructure/persistence/gorm/entities"
 	entity_mappers "auptex.com/botnova/internals/infrastructure/persistence/gorm/mappers"
 	"github.com/gofrs/uuid/v5"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -18,11 +19,20 @@ func NewUserRepository(db *gorm.DB) *UserRepository {
 
 func (r *UserRepository) Create(user models.User) error {
 	user.Id = uuid.Must(uuid.NewV7()).String()
+	bytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return err
+	}
+
+	user.Password = string(bytes)
+
 	entity := entity_mappers.ToUserEntity(user)
 	return r.db.Create(&entity).Error
+
 }
 
-func (r *UserRepository) GetById(id int) (*models.User, error) {
+func (r *UserRepository) GetById(id string) (*models.User, error) {
 	var entity entities.User
 	if err := r.db.First(&entity, "id = ?", id).Error; err != nil {
 		return nil, err
@@ -37,6 +47,6 @@ func (r *UserRepository) Update(user models.User) error {
 	return r.db.Save(&entity).Error
 }
 
-func (r *UserRepository) Delete(id int) error {
+func (r *UserRepository) Delete(id string) error {
 	return r.db.Delete(&entities.User{}, "id = ?", id).Error
 }
